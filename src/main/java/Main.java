51d2956023f1f032c5b27cdfc7e4256fa42821e9
Alejandro <alunaflores94@gmail.com>
2020-01-25 14:13:22 +0100

    subiendo el proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Main {
+
+	private static final String[] cmd = {
+			"java",
+			"-Xms2G",
+			"-Xmx2G",
+			"-jar",
+			"paper-75.jar",
+			"nogui"
+	};
+
+	private static final String directoryPath = "D:/Users/Alejandro/Documents/PaperCraft";
+
+	public static void main(String[] args) {
+//		process();
+		processBuilder();
+	}
+
+	private static void processBuilder() {
+		ProcessBuilder processBuilder = new ProcessBuilder(cmd);
+		processBuilder.directory(new File(directoryPath));
+		processBuilder.inheritIO();
+
+		try {
+			Process process = processBuilder.start();
+			process.waitFor();
+
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/MavenPlugin.java
@@ -0,0 +1,13 @@
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "start-server")
+public class MavenPlugin extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("MY PLUGIN");
+	}
+}
