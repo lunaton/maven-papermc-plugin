5dc5bbee7a8585940743aa311720f33cc8611c1d
Alejandro <alunaflores94@gmail.com>
2020-01-25 15:51:04 +0100

    version 0.0.4

--- src/main/java/com/gmail/lunaton/cmdexecutor/MavenPlugin.java
+++ src/main/java/com/gmail/lunaton/cmdexecutor/MavenPlugin.java
@@ -4,12 +4,32 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.io.IOException;
 
 @Mojo(name = "start-server")
 public class MavenPlugin extends AbstractMojo {
 
+	@Parameter(required = true)
+	private String command;
+
+	@Parameter(required = true)
+	private String workingDir;
+
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("MY PLUGIN");
+		ProcessBuilder processBuilder = new ProcessBuilder(command.split("\\s"));
+		processBuilder.directory(new File(workingDir));
+		processBuilder.inheritIO();
+
+		try {
+			Process process = processBuilder.start();
+			process.waitFor();
+
+		} catch (IOException | InterruptedException e) {
+			throw new MojoFailureException("a petao argo");
+		}
 	}
 }
